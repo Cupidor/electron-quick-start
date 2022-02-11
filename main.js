30215e477bdd2e11520c928d8911c532c88f486c
魏传鑫 <wcxin@njgn.com>
2022-02-11 14:37:27 +0800

    托盘化，禁止多线程打开

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, Menu } = require("electron");
+const { app, BrowserWindow, Menu, Tray } = require("electron");
 const path = require("path");
+let mainWindow = null,
+  tray = null
 
 function createWindow() {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 1000,
     height: 700,
     minWidth: 1000,
@@ -19,6 +21,41 @@ function createWindow() {
   // and load the index.html of the app.
   // mainWindow.loadFile("index.html");
   mainWindow.loadURL(path.join("file://", __dirname, "ant/index.html"));
+
+  // 触发关闭时触发
+  mainWindow.on('close', (event) => {
+    // 截获 close 默认行为
+    event.preventDefault();
+    // 点击关闭时触发close事件，我们按照之前的思路在关闭时，隐藏窗口，隐藏任务栏窗口
+    mainWindow.hide();
+    mainWindow.setSkipTaskbar(true);
+  });
+
+  // 新建托盘
+  tray = new Tray(path.join(__dirname, 'ant/icons/icon.ico'));
+  // 托盘名称
+  tray.setToolTip('Mint数据采集系统');
+  // 托盘菜单
+  const contextMenu = Menu.buildFromTemplate([{
+    label: '显示',
+    click: () => { mainWindow.show() }
+  },
+  {
+    label: '退出',
+    click: () => {
+      tray.destroy()
+      mainWindow.destroy()
+    }
+  }
+  ]);
+  // 载入托盘菜单
+  tray.setContextMenu(contextMenu);
+  // 双击触发
+  tray.on('double-click', () => {
+    // 双击通知区图标实现应用的显示或隐藏
+    mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
+    mainWindow.isVisible() ? mainWindow.setSkipTaskbar(false) : mainWindow.setSkipTaskbar(true);
+  });
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -48,7 +85,7 @@ function createWindow() {
           label: "联系我们",
           click() {
             require("electron").shell.openExternal(
-              "http://www.njgn.com/contact/contact.asp"
+              "https://www.njgn.com/aboutGn/contactus"
             );
           },
         },
@@ -59,47 +96,58 @@ function createWindow() {
   Menu.setApplicationMenu(menu);
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on("ready", function () {
-  runExec("start.bat"); // 生效啦，可以做些什么执行一种相对的同步状态，例如判断输出内容到什么了
-  createWindow();
-});
+const gotTheLock = app.requestSingleInstanceLock()
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // 当运行第二个实例时,将会聚焦到mainWindow这个窗口
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+      mainWindow.show()
+    }
+  })
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.on("ready", function () {
+    runExec("start.bat"); // 生效啦，可以做些什么执行一种相对的同步状态，例如判断输出内容到什么了
+    createWindow();
+  });
+  // Quit when all windows are closed.
+  app.on("window-all-closed", function () {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== "darwin") {
+      let stopProcess;
+      // 执行命令行，如果命令不需要路径，或就是项目根目录，则不需要cwd参数：
+      stopProcess = exec("stop.bat", {});
+      // 不受child_process默认的缓冲区大小的使用方法，没参数也要写上{}：workerProcess = exec(cmdStr, {})
+      // 打印正常的后台可执行程序输出
+      stopProcess.stdout.on("data", function (data) {
+        console.log("stdout: " + data);
+      });
 
-// Quit when all windows are closed.
-app.on("window-all-closed", function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
-    let stopProcess;
-    // 执行命令行，如果命令不需要路径，或就是项目根目录，则不需要cwd参数：
-    stopProcess = exec("stop.bat", {});
-    // 不受child_process默认的缓冲区大小的使用方法，没参数也要写上{}：workerProcess = exec(cmdStr, {})
-    // 打印正常的后台可执行程序输出
-    stopProcess.stdout.on("data", function (data) {
-      console.log("stdout: " + data);
-    });
+      // 打印错误的后台可执行程序输出
+      stopProcess.stderr.on("data", function (data) {
+        console.log("stderr: " + data);
+      });
 
-    // 打印错误的后台可执行程序输出
-    stopProcess.stderr.on("data", function (data) {
-      console.log("stderr: " + data);
-    });
-
-    // 退出之后的输出
-    stopProcess.on("close", function (code) {
-      //在bat执行完毕后再执行quit()
-      console.log("out code：" + code);
-      app.quit();
-    });
-  }
-});
-
-app.on("activate", function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow();
-});
+      // 退出之后的输出
+      stopProcess.on("close", function (code) {
+        //在bat执行完毕后再执行quit()
+        console.log("out code：" + code);
+        app.quit();
+      });
+    }
+  });
+  app.on("activate", function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+}
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
